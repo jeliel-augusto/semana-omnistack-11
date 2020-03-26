014774ea237fd69c9a8f622340713cebcc8aeb83
jeliel <jeliel.augusto10@gmail.com>
2020-03-26 16:06:11 -0400

    Mobile full implemented

---
+++ mobile/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from "@react-navigation/native";
+const AppStack = createStackNavigator();
+
+import Incidents from "./pages/incidents";
+import Detail from "./pages/detail";
+export default function Routes(){
+    return (
+      <NavigationContainer >
+          <AppStack.Navigator screenOptions={{headerShown: false}}>
+              <AppStack.Screen name="incidents" component={Incidents}/>
+              <AppStack.Screen name="detail" component={Detail} />
+          </AppStack.Navigator>
+      </NavigationContainer>
+    );
+}
